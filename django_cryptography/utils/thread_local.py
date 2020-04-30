82699ce9d03cc45837314adce712c50fb1723ce8
alex <alex@spotdraft.com>
2020-04-30 19:43:31 +0530

    Fix types

--- django_cryptography/utils/thread_local.py
+++ django_cryptography/utils/thread_local.py
@@ -1,8 +1,6 @@
 from threading import local
 from typing import Optional
 
-from cryptography.fernet import Fernet
-
 from django_cryptography.utils.crypto import FernetBytes
 
 _thread_locals = local()
@@ -10,7 +8,7 @@ _thread_locals = local()
 CRYPTER_KEY_ATTR_NAME = "_CRYPTER_KEY_ATTR_NAME"
 
 
-def set_encryption_key(encryption_key: Optional[str]):
+def set_encryption_key(encryption_key: Optional[bytes]):
     if encryption_key:
         crypter = FernetBytes(key=encryption_key)
     else:
@@ -18,5 +16,5 @@ def set_encryption_key(encryption_key: Optional[str]):
     setattr(_thread_locals, CRYPTER_KEY_ATTR_NAME, crypter)
 
 
-def get_crypter() -> Optional[Fernet]:
+def get_crypter() -> Optional[FernetBytes]:
     return getattr(_thread_locals, CRYPTER_KEY_ATTR_NAME, None)
